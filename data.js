6b44701f4d438715edcb09161a4d6fef3e7eb4d4
Humberto Hernandez <hamburger45@gmail.com>
2020-09-24 21:14:36 -0700

    started work for projects scripts

---
+++ data.js
@@ -0,0 +1,31 @@
+// Project data
+const project = [
+  {
+    title: "title 1",
+    description: "desc 1",
+    image: "image 1",
+    about: "about 1",
+    stack: ["html", "css", "javascript"],
+  },
+  {
+    title: "title 2",
+    description: "desc 2",
+    image: "image 2",
+    about: "about 2",
+    stack: ["html", "css", "javascript"],
+  },
+  {
+    title: "title 3",
+    description: "desc 3",
+    image: "image 3",
+    about: "about 3",
+    stack: ["html", "css", "javascript"],
+  },
+  {
+    title: "title 4",
+    description: "desc 4",
+    image: "image 4",
+    about: "about 4",
+    stack: ["html", "css", "javascript"],
+  },
+];
--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 // collapse navbar on click
-$(".navbar-nav>a").on("click", function () {
-  $(".navbar-collapse").collapse("hide");
-});
+function collapseNavBar() {
+  $(".navbar-nav>a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+}
 
 // smooth scroll
 let scroll = new SmoothScroll('a[href*="#"]', {
   speed: 800,
 });
+
+function insertProjectData() {
+  // get the data
+  // loop through data
+  // build the element
+  // apppend the element
+
+  // this test works , you need to refactor
+  let code =
+    '<div class=${"card"}></div>' +
+    '<div class="card-body">' +
+    "This is some text within a card block." +
+    "</div>";
+  $(".test-js").append(code);
+}
+
+// Control for running app, call functions on start up
+function masterControl() {
+  insertProjectData();
+  collapseNavBar();
+}
+
+$(masterControl);

f7653e92b497f462c261d2650547c9294493abf5
Humberto Hernandez <hamburger45@gmail.com>
2020-09-29 22:44:24 -0700

    working on project scripts

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const project = [
+  {
+    title: "title 1",
+    description: "desc 1",
+    image: "image 1",
+    about: "about 1",
+    stack: ["html", "css", "javascript"],
+  },
+  {
+    title: "title 2",
+    description: "desc 2",
+    image: "image 2",
+    about: "about 2",
+    stack: ["html", "css", "javascript"],
+  },
+  {
+    title: "title 3",
+    description: "desc 3",
+    image: "image 3",
+    about: "about 3",
+    stack: ["html", "css", "javascript"],
+  },
+  {
+    title: "title 4",
+    description: "desc 4",
+    image: "image 4",
+    about: "about 4",
+    stack: ["html", "css", "javascript"],
+  },
+];
 // collapse navbar on click
 function collapseNavBar() {
   $(".navbar-nav>a").on("click", function () {
@@ -16,6 +46,20 @@ function insertProjectData() {
   // build the element
   // apppend the element
 
+  STORE[questionNumber].answers.forEach(function (answerValue, answerIndex) {
+    $(`<label class="something" for="${answerIndex}">
+    <input class="radio" type="radio" id="${answerIndex}" value="${answerValue}" name="answer" required>
+    <span>${answerValue}</span>
+  </label>
+  `).appendTo($(".quizQA").find("fieldset"));
+  });
+
+  project.answers
+    .forEach((item) => {
+      $('<div class="card"></div><div class="card-body">${}</div>');
+    })
+    .appendTo($(".test-js"));
+
   // this test works , you need to refactor
   let code =
     '<div class=${"card"}></div>' +
@@ -23,6 +67,10 @@ function insertProjectData() {
     "This is some text within a card block." +
     "</div>";
   $(".test-js").append(code);
+
+  project.title.forEach((element) => {
+    console.log(element);
+  });
 }
 
 // Control for running app, call functions on start up

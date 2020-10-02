d40a42d557260f652bac69d1b837a79cbe9b986f
Humberto Hernandez <hamburger45@gmail.com>
2020-10-01 22:17:11 -0700

    added scripts for porjects section intial template in place

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-const project = [
+const PROJECTS = [
   {
     title: "title 1",
     description: "desc 1",
-    image: "image 1",
+    image: "./assets/logo.png",
     about: "about 1",
     stack: ["html", "css", "javascript"],
   },
   {
     title: "title 2",
     description: "desc 2",
-    image: "image 2",
+    image: "./assets/logo.png",
     about: "about 2",
     stack: ["html", "css", "javascript"],
   },
   {
     title: "title 3",
     description: "desc 3",
-    image: "image 3",
+    image: "./assets/logo.png",
     about: "about 3",
     stack: ["html", "css", "javascript"],
   },
   {
     title: "title 4",
     description: "desc 4",
-    image: "image 4",
+    image: "./assets/logo.png",
     about: "about 4",
     stack: ["html", "css", "javascript"],
   },
 ];
+
+// bug need to fix , wont collapse
 // collapse navbar on click
 function collapseNavBar() {
   $(".navbar-nav>a").on("click", function () {
@@ -46,37 +48,46 @@ function insertProjectData() {
   // build the element
   // apppend the element
 
-  STORE[questionNumber].answers.forEach(function (answerValue, answerIndex) {
-    $(`<label class="something" for="${answerIndex}">
-    <input class="radio" type="radio" id="${answerIndex}" value="${answerValue}" name="answer" required>
-    <span>${answerValue}</span>
-  </label>
-  `).appendTo($(".quizQA").find("fieldset"));
-  });
-
-  project.answers
-    .forEach((item) => {
-      $('<div class="card"></div><div class="card-body">${}</div>');
-    })
-    .appendTo($(".test-js"));
-
-  // this test works , you need to refactor
-  let code =
-    '<div class=${"card"}></div>' +
-    '<div class="card-body">' +
-    "This is some text within a card block." +
-    "</div>";
-  $(".test-js").append(code);
-
-  project.title.forEach((element) => {
-    console.log(element);
+  PROJECTS.forEach((item) => {
+    // item.key to get data
+    $(".test-js").append(
+      `<div class="container col-lg">
+      <div class="card">
+        <img
+          class="card-img-top"
+          src="${item.image}"
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+          <p class="card-text">${item.description}</p>
+          <div
+            class="btn-group"
+            role="group"
+            aria-label="project-buttons"
+          >
+            <button type="button" class="btn btn-secondary">
+              Demo
+            </button>
+            <button type="button" class="btn btn-secondary">
+              Code
+            </button>
+            <button type="button" class="btn btn-secondary">
+              About
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </div>`
+    );
   });
 }
 
 // Control for running app, call functions on start up
 function masterControl() {
   insertProjectData();
-  collapseNavBar();
+  // collapseNavBar();
 }
 
 $(masterControl);
